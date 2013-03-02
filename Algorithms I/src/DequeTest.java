6c98fd0177845c83cef7f2ce593dd16d321ed3e2
dsullivan <djcsdjcs@gmail.com>
2013-03-02 00:34:53 -0600

    Adding Deque and Randonized queue with tests.

---
+++ Algorithms I/src/DequeTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DequeTest {
+
+}
---
+++ Algorithms I/src/Subset.java
@@ -0,0 +1,14 @@
+
+public class Subset {
+  /**
+   * Subset client. Write a client program Subset.java that takes a command-line
+   * integer k, reads in a sequence of N strings from standard input using
+   * StdIn.readString(), and prints out exactly k of them, uniformly at random.
+   * Each item from the sequence can be printed out at most once. You may assume
+   * that k ³ 0 and no greater than the number of string on standard input. 
+   * @param args
+   */
+  public static void main(String[] args) {
+  }
+
+}
