b398185c4726c0cd6cb99da8ae3651b286a80a0c
dsullivan <djcsdjcs@gmail.com>
2013-03-04 00:52:11 -0600

    Warming up for making a double-ended queue. Adding code for LinkedQueue and ArrayQueue, for strings, then the general case.

---
+++ Algorithms I/src/ArrayQueueOfStringsTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+public class ArrayQueueOfStringsTest {
+  @Test
+  public void testArrayQueueOfStrings() {
+    ArrayQueueOfStrings queue = new ArrayQueueOfStrings();
+    queue.enqueue("one");
+    queue.enqueue("two");
+    queue.enqueue("three");
+
+    assertEquals("one", queue.dequeue());
+    assertEquals("two", queue.dequeue());
+    assertEquals("three", queue.dequeue());
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testNullItem() throws Exception {
+    new ArrayQueueOfStrings().enqueue(null);
+  }
+
+  @Test(expected = NoSuchElementException.class)
+  public void testDequeueEmptyQueue() throws Exception {
+    new ArrayQueueOfStrings().dequeue();
+  }
+}
---
+++ Algorithms I/src/LinkedStackOfStringsTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+public class LinkedStackOfStringsTest {
+  @Test
+  public void testArrayStackOfStrings() {
+    ArrayStackOfStrings stack = new ArrayStackOfStrings();
+    stack.push("item1");
+    stack.push("item2");
+    stack.push("item3");
+
+
+    assertEquals("item3", stack.pop());
+    assertEquals("item2", stack.pop());
+    assertEquals("item1", stack.pop());
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testPushNullItem() throws Exception {
+    new ArrayStackOfStrings().push(null);
+  }
+
+  @Test(expected = NoSuchElementException.class)
+  public void testPopEmptyQueue() throws Exception {
+    new ArrayStackOfStrings().pop();
+  }
+}
