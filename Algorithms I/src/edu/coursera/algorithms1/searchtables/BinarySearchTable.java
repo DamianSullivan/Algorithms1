bb26fd94b49da4a12b68046a43797e5b3c6db8b6
dsullivan <djcsdjcs@gmail.com>
2013-12-07 04:36:21 -0600

    Adding implementation for BinarySearchTable.

--- Algorithms I/src/edu/coursera/algorithms1/searchtables/BinarySearchTable.java
+++ Algorithms I/src/edu/coursera/algorithms1/searchtables/BinarySearchTable.java
@@ -12,47 +12,99 @@ public class BinarySearchTable<Key extends Comparable<Key>, Value> implements Se
     vals = (Value[]) new Object[capacity];
   }
 
-  public int size() {
-    return N;
+  // For testing.
+  protected BinarySearchTable(Key[] keys, Value[] vals) {
+    this.keys = keys;
+    this.vals = vals;
+    N = keys.length + 1;
   }
-
-  public Value get(Key key) {
-    if (isEmpty())
-      return null;
-    int i = rank(key);
-    if (i < N && keys[i].compareTo(key) == 0)
-      return vals[i];
-    else
-      return null;
-  }
-
-  public boolean isEmpty() {
-    return N > 1;
-  }
-
-  public int rank(Key key) {
-    // See page 381.
-    return 0;
-  }
-
+  
   // Search for key. Update value if found; grow table if new.
   public void put(Key key, Value val) {
-    int i = rank(key);
-    if (i < N && keys[i].compareTo(key) == 0) {
-      vals[i] = val;
+    // Where should this key go in the table?
+    int rank = rank(key);
+    
+    // Update the value if same key already exist at this rank.
+    if (rank < N && keys[rank].compareTo(key) == 0) {
+      vals[rank] = val;
       return;
     }
-    for (int j = N; j > i; j--) {
-      keys[j] = keys[j - 1];
-      vals[j] = vals[j - 1];
+    
+    // Grow the table by one to fit the new key.
+    for (int j = N; j > rank; j--) {
+      keys[j] = keys[j-1];
+      vals[j] = vals[j-1];
     }
-    keys[i] = key;
-    vals[i] = val;
+    
+    // Insert the new key into the table.
+    keys[rank] = key;
+    vals[rank] = val;
+    
+    // Update N to reflect the new size of the table.
     N++;
   }
-
+  
+  public Value get(Key key) {
+    // If this table is empty then there is no key.
+    if (isEmpty()) {
+      return null;
+    }
+    
+    // Search for this key and get the index of where it should be in the table.
+    int rank = rank(key);
+    
+    // Return the key if it exists where it should be.
+    if (rank < N && keys[rank].compareTo(key) == 0) {
+      return vals[rank];
+    } else {
+      return null;
+    }
+  }
+  
   public void delete(Key key) {
-    // See Exercise 3.1.16 for this method.
+    if (isEmpty()) {
+      return;
+    }
+    
+    // Find the key should be in the table.
+    int rank = rank(key);
+    
+    // Key is not in the table.
+    if (rank == N || keys[rank].compareTo(key) != 0) {
+      return;
+    }
+        
+    // Remove the element by collapsing the table by one.
+    for (int j = rank; j < N-1; j++) {
+      keys[j] = keys[j+1];
+      vals[j] = vals[j+1];
+    }
+    
+    // Erase the last element since we collapsed the table by one.
+    keys[N] = null;
+    vals[N] = null;
+    
+    // Update N to reflect the new size of the table.
+    N--;
+    
+    
+    // Resize the table if only 1/4 full.
+    if (N > 0 && N == keys.length/4) {
+      resize(keys.length/2);
+    }
+  }
+  
+  @SuppressWarnings("unchecked")
+  private void resize(int size) {
+    assert size >= N;
+    Key[] resizedKeys = (Key[]) new Comparable[size];
+    Value[] resizedValues = (Value[]) new Object[size];
+    for (int i = 0; i < N; i++) {
+      resizedKeys[i] = keys[i];
+      resizedValues[i] = vals[i];
+    }
+    keys = resizedKeys;
+    vals = resizedValues;
   }
 
   @Override
@@ -61,6 +113,14 @@ public class BinarySearchTable<Key extends Comparable<Key>, Value> implements Se
     return false;
   }
 
+  public boolean isEmpty() {
+    return N == 0;
+  }
+  
+  public int size() {
+    return N;
+  }
+  
   @Override
   public Key min() {
     // TODO Auto-generated method stub
@@ -83,6 +143,46 @@ public class BinarySearchTable<Key extends Comparable<Key>, Value> implements Se
   public Key ceiling(Key key) {
     // TODO Auto-generated method stub
     return null;
+  }
+
+  /**
+   * If the key is in the table, rank() returns its index in the table,
+   * which is the same as the number of keys in the table that are smaller than the key.
+   * 
+   * If key is not in the table, rank() also returns the number of keys in the table that are
+   * smaller than key.
+   */
+  public int rank(Key key) {
+    int lo = 0; 
+    int hi = N - 1;
+
+    while (lo <= hi) {
+      // Find the middle of the array within the current interval.
+      int mid = lo + (hi - lo) / 2;
+      
+      // Is the key >, <, or = to the middle key?
+      // compareTo returns:
+      //     * -1 for less than
+      //     *  0 for equal to
+      //     *  1 for greater than
+      int cmp = key.compareTo(keys[mid]);
+      
+      // The key is less than middle, then it is somewhere in the lower half of the range.
+      // Set hi to one less than the middle as an upper bound to the next search.
+      if (cmp < 0) {
+        hi = mid - 1;
+      }
+      // The key is greater than the middle, it is somewhere in the upper half of the range.
+      // Set lo to one more than the middle as an upper bound to the next search.
+      else if (cmp > 0) {
+        lo = mid + 1;
+      }
+      // The key is equal to the middle. Return the index of the key which is mid.
+      else {
+        return mid;
+      }
+    }
+    return lo;
   }
 
   @Override
--- test/edu/coursera/algorithms1/searchtables/BinarySearchTableTest.java
+++ test/edu/coursera/algorithms1/searchtables/BinarySearchTableTest.java
@@ -1,5 +1,8 @@
 package edu.coursera.algorithms1.searchtables;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
@@ -11,17 +14,49 @@ public class BinarySearchTableTest {
 
   @Test
   public void testPut() {
-    fail("Not yet implemented");
+    Integer[] keys = new Integer[3];
+    String[] vals = new String[3];
+    BinarySearchTable<Integer, String> binarySearchTable =
+        new BinarySearchTable<Integer, String>(keys, vals);
+    
+    binarySearchTable.put(1, "one");
+    binarySearchTable.put(2, "two");
+    binarySearchTable.put(3, "three");
+    
+    Integer[] expectedKeys = {1, 2, 3};
+    String[] expectedVals = {"one", "two", "three"};
+    assertArrayEquals(expectedKeys, keys);
+    assertArrayEquals(expectedVals, vals);
   }
   
   @Test
   public void testGet() {
-    fail("Not yet implemented");
+    Integer[] keys = {1, 2, 3};
+    String[] vals = {"one", "two", "three"};
+    BinarySearchTable<Integer, String> binarySearchTable =
+        new BinarySearchTable<Integer, String>(keys, vals);
+    assertEquals("one", binarySearchTable.get(1));
+    assertEquals("two", binarySearchTable.get(2));
+    assertEquals("three", binarySearchTable.get(3));
   }
   
   @Test
-  public void testDelete() {
-    fail("Not yet implemented");
+  public void testDeleteResizesKeyArray() {
+    Integer[] keys = {1, 2, 3, 4};
+    String[] vals = {"one", "two", "three", "four"};
+    BinarySearchTable<Integer, String> binarySearchTable =
+        new BinarySearchTable<Integer, String>(keys, vals);
+    
+    binarySearchTable.delete(2);
+    //binarySearchTable.delete(3);
+    //binarySearchTable.delete(4);
+
+    //assertEquals("one", binarySearchTable.get(1));
+    assertNull(binarySearchTable.get(2));
+    //assertNull(binarySearchTable.get(3));
+    //assertNull(binarySearchTable.get(4));
+    //assertEquals(1, keys.length);
+    //assertEquals(1, vals.length);
   }
   
   @Test
