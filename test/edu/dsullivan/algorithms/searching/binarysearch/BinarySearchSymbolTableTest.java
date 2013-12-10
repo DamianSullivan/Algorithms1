483c9fd75e75aebfde60c785e274818c22a5d22c
dsullivan <djcsdjcs@gmail.com>
2013-12-10 14:05:26 -0600

    Renaming binarysearch test package for clarity.

--- test/edu/dsullivan/algorithms/searching/binarysearch/BinarySearchSymbolTableTest.java
+++ test/edu/dsullivan/algorithms/searching/binarysearch/BinarySearchSymbolTableTest.java
@@ -1,4 +1,4 @@
-package edu.dsullivan.algorithms.binarysearch;
+package edu.dsullivan.algorithms.searching.binarysearch;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
